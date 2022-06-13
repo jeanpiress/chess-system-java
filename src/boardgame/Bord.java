1174036ccb8b9f59e6646993fb4c1f526174c546
jeanpiress <jean.m.pires@gmail.com>
2022-06-13 13:44:15 -0300

    BordExcepition and defensive programing

--- src/boardgame/Bord.java
+++ src/boardgame/Bord.java
@@ -8,6 +8,9 @@ public class Bord {
 	
      
      public Bord(Integer rows, Integer columns) {
+    	 if(rows < 0 || columns < 0 ) {
+    		 throw new BordException("Error creating bord: there must be at least 1 row and 1 column");
+    	 }
 		this.rows = rows;
 		this.columns = columns;
         pieces = new Piece[rows][columns];	
@@ -18,33 +21,45 @@ public class Bord {
 		return rows;
 	}
 
-
-	public void setRows(Integer rows) {
-		this.rows = rows;
-	}
-
-
 	public Integer getColumns() {
 		return columns;
 	}
 
-
-	public void setColumns(Integer columns) {
-		this.columns = columns;
-	}
-     
-     public Piece piece(int rows, int columns) {
-    	 return pieces[rows][columns];
+     public Piece piece(int row, int column) {
+    	if(!positionExists(row, column)) {
+    		throw new BordException("position not on the bord");
+    		
+    	}
+    	 return pieces[row][column];
      }
      
      public Piece piece(Position position) {
+    	 if(!positionExists(position)) {
+     		throw new BordException("position not on the bord");
+    	 }
     	 return pieces[position.getRow()][position.getColumn()];
      }
      
     public void placePiece(Piece piece, Position position) {
+    	if(thereIsAPiece(position)) {
+    		throw new BordException("there is already a piece on position " + position);
+    	}
     	pieces[position.getRow()][position.getColumn()] = piece;
     	piece.position = position;
     }
      
+    private boolean positionExists(int row, int column) {
+    	return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
      
+    public boolean positionExists(Position position) {
+    	return positionExists(position.getRow(), position.getColumn());
+    }
+    
+    public boolean thereIsAPiece(Position position) {
+    	 if(!positionExists(position)) {
+      		throw new BordException("position not on the bord");
+     	 }
+    	return piece(position) != null;
+    }
 }
---
+++ src/boardgame/BordException.java
@@ -0,0 +1,13 @@
+package boardgame;
+
+public class BordException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+     public BordException(String msg) {
+    	 super(msg);
+     }
+
+
+
+
+}
